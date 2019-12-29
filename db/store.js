b70b05f6c278d04c4d12d458c0ddcf6a7e771eaa
osborw <jacobwayne1513@aol.com>
2019-12-29 13:54:26 -0500

    some sql changes

--- db/store.js
+++ db/store.js
@@ -5,13 +5,13 @@ export const getTop50 = async position => {
   let q
   if (position === 'FLEX') {
     q = `
-    SELECT s.id, p.name, p.position, s.pts_ppr/s.gms_active AS avg_points, p.owner_id
+    SELECT s.id, p.name, p.position, s.gp, s.pts_ppr/s.gp AS avg_points, s.std_dev, p.owner_id
     FROM season s INNER JOIN players p ON s.id = p.id
-    WHERE (p.position = 'RB' OR p.position = 'WR' OR p.position = 'TE') AND s.gms_active > 0
+    WHERE (p.position = 'RB' OR p.position = 'WR' OR p.position = 'TE') AND s.games_active > 0
     ORDER BY avg_points DESC LIMIT 100;`
   } else {
     q = `
-    SELECT s.id, p.name, s.pts_ppr/s.gms_active AS avg_points, p.owner_id
+    SELECT s.id, p.name, s.gp, s.pts_ppr/s.gp AS avg_points, s.std_dev, p.owner_id
     FROM season s INNER JOIN players p ON s.id = p.id
     WHERE p.position = '${position}' AND s.gms_active > 0
     ORDER BY avg_points DESC LIMIT 50;`
@@ -30,8 +30,9 @@ export const getTop50 = async position => {
       id: p['id'],
       name: p['name'],
       position: p['position'],
-      gamesPlayed: p['games_played'],
+      gamesPlayed: p['gp'],
       avgPoints: p['avg_points'],
+      stdDev: p['std_dev'],
       ownerId: p['owner_id']
     }
   })
@@ -44,7 +45,7 @@ export const getFiveWeekTop50 = async position => {
   if (position === 'FLEX') {
     q = `
     SELECT w.id, p.name, p.position, p.owner_id, COUNT(DISTINCT w.week) AS games_played, SUM(DISTINCT w.pts_ppr)/COUNT(DISTINCT w.week) AS avg_points
-    FROM ((SELECT * FROM JSXR.weeks WHERE week >= 9) AS w INNER JOIN JSXR.players p ON w.id = p.id)
+    FROM ((SELECT * FROM JSXR.weeks WHERE week >= 11) AS w INNER JOIN JSXR.players p ON w.id = p.id)
     WHERE p.position = 'rb' OR p.position = 'te' OR p.position = 'wr'
     GROUP BY w.id, p.name, p.position, p.owner_id
     ORDER BY avg_points DESC LIMIT 100;
@@ -52,7 +53,7 @@ export const getFiveWeekTop50 = async position => {
   } else {
     q = `
     SELECT w.id, p.name, p.position, p.owner_id, COUNT(DISTINCT w.week) AS games_played, SUM(DISTINCT w.pts_ppr)/COUNT(DISTINCT w.week) AS avg_points
-    FROM ((SELECT * FROM JSXR.weeks WHERE week >= 9) AS w INNER JOIN JSXR.players p ON w.id = p.id)
+    FROM ((SELECT * FROM JSXR.weeks WHERE week >= 11) AS w INNER JOIN JSXR.players p ON w.id = p.id)
     WHERE p.position = '${position}'
     GROUP BY w.id, p.name, p.position, p.owner_id
     ORDER BY avg_points DESC LIMIT 50;
@@ -78,5 +79,22 @@ export const getFiveWeekTop50 = async position => {
     }
   })
 
+  return ret
+}
+
+export const getWeeks = async (id, start) => {
+  const q = `SELECT pts_ppr, week FROM JSXR.weeks WHERE id = '${id}' AND week >= ${start};`
+  const res = await query(q)
+  if (!res) {
+    console.log('there aint no res!')
+    return
+  }
+
+  const data = res[0]
+
+  const ret = data.map(w => ({
+    ptsPPR: w['pts_ppr'],
+    weekNumber: w['week']
+  }))
   return ret
 }
--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 import * as Store from './db/store'
 const fastify = require('fastify')({ logger: true })
 
-fastify.register(require('fastify-cors'), { 
-  origin: true
+fastify.register(require('fastify-cors'), {
+  origin: true,
 })
 
 fastify.get('/allSeason/:position', async (request, reply) => {
   console.log('--Call made from', request.hostname, '--')
-  return await Store.getTop50(request.params.position)
+  const top50 = await Store.getTop50(request.params.position)
+  const ret = Promise.all(
+    top50.map(async p => ({
+      weeks: await Store.getWeeks(p.id, 1),
+      ...p,
+    })),
+  )
+  console.log(ret)
+  return ret
 })
 
 fastify.get('/fiveWeeks/:position', async (request, reply) => {
   console.log('--Call made from', request.hostname, '--')
-  return await Store.getFiveWeekTop50(request.params.position)
+  const top50 = await Store.getFiveWeekTop50(request.params.position)
+  const startWeek = 9
+  const ret = Promise.all(
+    top50.map(async p => ({
+      weeks: await Store.getWeeks(p.id, startWeek),
+      ...p,
+    })),
+  )
+  return ret
 })
 
 const start = async () => {
